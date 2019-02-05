a1864d0a89cab1a7b0645a5bc04df42e2c95bd4b
Garrett Richards <garrettxaivier@gmail.com>
2019-02-04 16:18:28 -0800

    completed listings backend

--- server/routes/loo.js
+++ server/routes/loo.js
@@ -2,7 +2,6 @@ import express from 'express';
 const Router = express.Router();
 import conn from '../utils/db'
 
-
 Router.get('/listings', (req, res, next) => {
     const sql = `SELECT a.*, GROUP_CONCAT(feat.feature SEPARATOR ', ') as features
     FROM listings a
@@ -17,5 +16,20 @@ Router.get('/listings', (req, res, next) => {
             res.json(results);
         });
 });
+
+Router.get('/listings/:id', (req, res, next) => {
+    const sql = `SELECT a.*, GROUP_CONCAT(feat.feature SEPARATOR ', ') as features
+    FROM listings a
+    LEFT JOIN listings_features_link b ON a.id = b.listing_id
+    LEFT JOIN features feat ON b.feature_id = feat.id
+    WHERE a.id = ?
+    GROUP BY a.id;`
+    const id = req.params.id
+
+    conn.query(sql, [id], (err, results, fields) => {
+        console.log(results)
+        res.json(results)
+      })
+})
 
 export default Router
